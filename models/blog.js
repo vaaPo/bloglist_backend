74e3b3dfd9eb894a79d954bfbd8ee9786213835f
vaaPo <ripraptiptop@gmail.com>
2018-12-13 15:18:08 +0200

    blog.js - model for blogs

--- models/blog.js
+++ models/blog.js
@@ -1,4 +1,5 @@
 //to model
+/**
 const Blog = mongoose.model('Blog', {
   title: String,
   author: String,
@@ -7,3 +8,31 @@ const Blog = mongoose.model('Blog', {
 });
 
 module.exports = Blog;
+ */
+// ***********
+//FIXME in index.js const Note = require('./models/notes')
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+//    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+blogSchema.statics.format = (blog) => {
+  return {
+    id: blog._id,
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes
+    //    blogs: blog.user
+  };
+};
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
+// call me Blog.format(blog)
